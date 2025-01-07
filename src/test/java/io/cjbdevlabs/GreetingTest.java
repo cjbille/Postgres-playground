d417e478b9e6a1350550ea914a498e234086c128
cjbille <102840549+cjbille@users.noreply.github.com>
2025-01-07 11:25:46 +0000

    WIP

--- src/test/java/io/cjbdevlabs/GreetingTest.java
+++ src/test/java/io/cjbdevlabs/GreetingTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 class GreetingTest {
@@ -14,7 +14,7 @@ class GreetingTest {
           .when().get("/greeting")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(containsString("Hello"));
     }
 
 }
