b3c1defd0948a94288e5ac32ddeeeb401fbb58b3
cjbille <102840549+cjbille@users.noreply.github.com>
2025-01-20 15:55:49 +0000

    Updates

--- src/main/java/io/cjbdevlabs/Main.java
+++ src/main/java/io/cjbdevlabs/Main.java
@@ -1,8 +1,10 @@
 package io.cjbdevlabs;
 
+import io.cjbdevlabs.library.Book;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.transaction.Transactional;
 import lombok.extern.jbosslog.JBossLog;
 
 @JBossLog
@@ -15,7 +17,11 @@ public class Main {
     public static class MyApp implements QuarkusApplication {
         
         @Override
+        @Transactional
         public int run(String... args) throws Exception {
+            Book book = Book.find("isbn = ?1", "978-0358439196").firstResult();
+            log.infof("found book : %s", book);
+
             log.info("Starting Quarkus App");
             Quarkus.waitForExit();
             return 0;
--- src/main/java/io/cjbdevlabs/library/Book.java
+++ src/main/java/io/cjbdevlabs/library/Book.java
@@ -4,9 +4,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 @Entity
 public class Book extends PanacheEntity {
 
